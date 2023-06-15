1aa0ace27515d67926d747b2eedcde1e72bdf0b2
ashbake <ashbake@users.noreply.github.com>
2023-06-15 13:22:15 -0700

    add requirements setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def get_requires():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
+setup(
+    name='specsim',
+    version='0.1',
+    description='specsim: an SNR calculator for upcoming AO instruments',
+    url='https://github.com/ashbake/specsim',
+    author='Ashley Baker',
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        ],
+    install_requires=get_requires()
+    )
